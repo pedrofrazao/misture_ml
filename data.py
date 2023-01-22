43f673d5f32182d2bd7e5688e7cc4aa06dd12577
pedro.frazao <apf-26@netcf.org>
2023-01-22 18:13:46 +0000

    procedure to validate data

--- data.py
+++ data.py
@@ -39,9 +39,39 @@ def read_from_csv_to_array( filename ):
             entries.append( ar )
     return np.array( entries, dtype=object)
 
+
+def check_array_type_on_array_class( ad ):
+    """check if all values are of the same type on the array"""
+    error = None
+    class_sum = ad[0].sum()
+    for i in range(1,len(ad)):
+        sum=ad[i].sum()
+        if(class_sum != sum):
+            error=f'found a different sum for class in position {i+1}: got {sum} and expecting {class_sum}'
+            break
+    return error
+
+
+def check_array_type_values( ad ):
+    """check if all values are of the same type on the array"""
+    error = None
+    vtype = type( ad[0] )
+    if( vtype == np.ndarray ):
+        return check_array_type_on_array_class( ad )
+    for i in range(1,len(ad)):
+        if( type(ad[i]) != vtype ):
+            error=f'found a different type on array in position {i+1}'
+            break
+    return error
+
+
 def check_array_data( ad ):
     """check that the array have the proper format"""
-    pass
+    r,c=ad.shape
+    for i in range(0,c):
+        err = check_array_type_values( ad[:,i] )
+        if(err is not None):
+            print(f'ERROR: on column {i} - {err}')
 
 if __name__ == "__main__":
     f = "data1.csv"
@@ -49,3 +79,4 @@ if __name__ == "__main__":
         f = sys.argv[1]
     r = read_from_csv_to_array( f )
     print(r)
+    check_array_data( r )
